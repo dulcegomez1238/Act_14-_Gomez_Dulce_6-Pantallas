33d9450059192043316192ad8f9763794b28bd5d
dulcegomez1238 <a.22320051500080@cbtis128.edu.mx>
2026-03-04 11:38:40 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/pantallas/1pagina.dart';
+import 'package:myapp/pantallas/2pagina.dart';
+import 'package:myapp/pantallas/3pagina.dart';
+import 'package:myapp/pantallas/4pagina.dart';
+import 'package:myapp/pantallas/5pagina.dart';
+import 'package:myapp/pantallas/6pagina.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -16,6 +22,13 @@ class MyApp extends StatelessWidget {
       ),
       home: LoginPage(),
       debugShowCheckedModeBanner: false,
+      //mapa de rutas
+      '/':(context) => const 1pagina(),
+       'segubda':(context) => const 2pagina(),
+      'tercera':(context) => const 3pagina(),
+      'cuarta':(context) => const 4pagina(),
+      'quinta':(context) => const 5pagina(),
+      'sexta':(context) => const 6pagina(),
     );
   }
 }
